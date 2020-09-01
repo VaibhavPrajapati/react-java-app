e8759731165798367e9a9b8425f79b775bdebc9c
Vaibhav Prajapati <vaibhavprajapati1@gmail.com>
2020-09-01 22:02:53 +0530

    added cors logic

--- springboot-backend/src/main/java/net/javaguides/springboot/SpringbootBackendApplication.java
+++ springboot-backend/src/main/java/net/javaguides/springboot/SpringbootBackendApplication.java
@@ -3,6 +3,11 @@ package net.javaguides.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class SpringbootBackendApplication extends SpringBootServletInitializer {
@@ -10,5 +15,19 @@ public class SpringbootBackendApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBackendApplication.class, args);
 	}
+	
+	
+	@Bean
+	public CorsFilter corsFilter() {
+	    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    final CorsConfiguration config = new CorsConfiguration();
+	    config.setAllowCredentials(true);
+	    config.setAllowedOrigins(Arrays.asList("*"));
+	    config.setAllowedHeaders(Arrays.asList("*"));
+	    config.setAllowedMethods(Arrays.asList("*"));
+	    source.registerCorsConfiguration("/**", config);
+	    return new CorsFilter(source);
+	}
+
 
 }
